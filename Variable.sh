4751c0b7f1088ffca7438d19f60adbbbd6f36531
Vikas-git <Vikasshetty543@gmail.com>
2025-03-02 17:35:29 +0530

    Variable in bash

---
+++ Variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+t="uptime"
+p="pwd"
+
+
+echo "Uptime is displayed"
+$t
+$p
+## declaring the variables in bash scripting .there are two types of variable system variable (set command) and userdefine variable.
