44b0255261902a876fccb5cda2ebe5e534d3ea2d
Vikas-git <Vikasshetty543@gmail.com>
2025-03-09 17:19:27 +0530

    Create ifcondition4.sh

---
+++ ifcondition4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+string2="apple"
+string1="orange"
+
+if [ "$string1" == " $string2" ]   ## this is campare the string if it is match the condition is true and if not condition is not satsified.
+then
+
+echo " they are matching element"
+else
+
+echo " they are not  matching element"
+fi
