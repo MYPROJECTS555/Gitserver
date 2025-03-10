c0452966e071642730c8bd82a02d263991b88dad
Vikas-git <Vikasshetty543@gmail.com>
2025-03-10 22:31:48 +0530

    Update Checkstatusapplication.sh
    path added

--- Checkstatusapplication.sh
+++ Checkstatusapplication.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 status=$(systemctl status docker| awk '/Active/{print $3}' | tr -d "[()]")  # this command print out :  Status of the docker: running
+path=$(pwd)
 echo " Status of the docker:$status"
+echo "$path"
