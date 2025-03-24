1dd0508f37002879a3782d240cf2fd75422a31e5
Vikas-git <Vikasshetty543@gmail.com>
2025-03-24 17:49:11 +0530

    Update and rename Dockerinstall.sh to DockerUbantuInstall.sh

--- DockerUbantuInstall.sh
+++ DockerUbantuInstall.sh
@@ -41,6 +41,10 @@ echo << EOF
 "=========================================================="
 EOF
 
+
+### it is for ubantu server
+
+
 dockerStatus=$(systemctl status docker | awk '/Active/ {print $3}' | tr -d "[()]")
 dockerVersion=$(docker -v | awk '/version/ {print $3}' | tr -d ",")
 
