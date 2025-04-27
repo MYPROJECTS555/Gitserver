9839327fab55f12b6500cf5652aef0bc90d6adf0
MYPROJECTS555 <vikasshetty543@gmail.com>
2025-04-27 11:26:13 +0000

    frist commit

--- jenkinsfile_mvn.sh
+++ jenkinsfile_mvn.sh
@@ -1,1 +1,34 @@
+pipeline 
+{
+ agent any
+ tools
+  {
+  jdk 'java-11'
+  maven 'maven'
+  }
 
+ stages
+   {
+   stage('code-checkout')
+   {
+   steps
+   {
+   git branch:'master',url:'https://github.com/shashirajraja/shopping-cart'
+   }
+   }
+   stage('mvn compile')
+   {
+   steps
+   {
+   sh 'mvn compile'
+   }
+   }
+   stages('mvn install')
+   {
+   steps
+   {
+   sh 'mvn clean install'
+   }
+   }  
+   }
+}
